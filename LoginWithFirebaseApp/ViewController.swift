18033cc78874f6c78a51a60aa6e7bd455f0d8b80
yang <heroparkjisung2gmail.com>
2020-04-08 11:23:01 +0900

    Initial Commit

---
+++ LoginWithFirebaseApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoginWithFirebaseApp
+//
+//  Created by Y on 2020/04/08.
+//  Copyright © 2020 HEETAE YANG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
